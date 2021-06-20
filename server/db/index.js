d29adfcce1399f4f56d089874000dcf289d27fe6
Lukas <lukyn.odehnal333@gmail.com>
2021-06-20 10:18:51 +0200

    .gitignore fix1

---
+++ server/db/index.js
@@ -0,0 +1,12 @@
+const { Pool } = require('pg')
+
+const pool = new Pool({
+  user: 'dbuser',
+  host: 'database.server.com',
+  database: 'mydb',
+  password: 'secretpassword',
+  port: 3211,
+})
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+} 
