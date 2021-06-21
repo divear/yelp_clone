a4950584a812510b6666323f9f2a0b02903cf0d9
Lukas <lukyn.odehnal333@gmail.com>
2021-06-21 17:00:04 +0200

    create route

--- server/server.js
+++ server/server.js
@@ -15,35 +15,56 @@ app.use((req,res, next)=>{
 
 //get all
 app.get("/api/v1/restaurants", async(req, res)=>{
-    const results = await db.query("SELECT * FROM restaurants")
-    console.log(results);
-    res.json({
-        status: "success",
-        data: {
-            restaurants: results[rows] 
-        }
-    });
+    try {
+        const results = await db.query("SELECT * FROM restaurants");
+        console.log(results);
+        res.json({
+            status: "success",
+            results: results.rows.length,
+            data: {
+                restaurants: results.rows
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
+  
 });
 //get individual
-app.get("/api/v1/restaurants/:id", (req, res)=>{
-    console.log(req.params);
-    res.status(200).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonald offbrand meal"
-        }
-    })
+app.get("/api/v1/restaurants/:id", async(req, res)=>{
+    console.log(req.params.id);
+    try {
+        const results = await db.query("SELECT * FROM restaurants WHERE id = $1", [req.params.id]);
+        console.log(results.rows[0]);
+
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0],
+            },
+        })
+    } catch (error) {
+        console.log(error);
+    }
+   
 });
 
 //create
-app.post("/api/v1/restaurants/", (req, res)=>{
-    console.log(req);
-    res.status(201).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonald offbrand meal"
-        }
-    })
+app.post("/api/v1/restaurants/", async(req, res)=>{
+    try {
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1,$2, $3)",[req.body.name, req.body.location, reg.body.price_range]);
+        console.log(req);
+        res.status(201).json({
+            status: "success",
+            data: {
+                restaurant: "mcdonald offbrand meal"
+            }
+        })
+    } catch (error) {
+        console.log(error);
+    }
+    
 })
 
 //update
