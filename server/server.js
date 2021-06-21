ada9d62ee6451fce64be740c81f889466e43e209
Lukas <lukyn.odehnal333@gmail.com>
2021-06-21 18:55:45 +0200

    backend done

--- server/server.js
+++ server/server.js
@@ -53,12 +53,12 @@ app.get("/api/v1/restaurants/:id", async(req, res)=>{
 //create
 app.post("/api/v1/restaurants/", async(req, res)=>{
     try {
-        const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1,$2, $3)",[req.body.name, req.body.location, reg.body.price_range]);
-        console.log(req);
+        const results = await db.query("INSERT INTO restaurants (name, location, price_range) VALUES ($1,$2, $3) RETURNING *",[req.body.name, req.body.location, req.body.price_range]);
+        console.log(results);
         res.status(201).json({
             status: "success",
             data: {
-                restaurant: "mcdonald offbrand meal"
+                restaurant: results.rows[0]
             }
         })
     } catch (error) {
@@ -68,20 +68,35 @@ app.post("/api/v1/restaurants/", async(req, res)=>{
 })
 
 //update
-app.put("/api/v1/restaurants/:id", (req, res)=>{
-    console.log(req.params.id);
-    res.status(200).json({
-        status: "success",
-    })  
+app.put("/api/v1/restaurants/:id", async(req, res)=>{
+    try {
+        const results = await db.query("UPDATE restaurants SET name = $1, location = $2, price_range = $3 WHERE id = $4 RETURNING *", [req.body.name, req.body.location, req.body.price_range, req.params.id ]);
+        res.status(200).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+        console.log(results); 
+    } catch (error) {
+        console.log(error);
+    }
+     
 })
 
-app.delete("/api/v1/restaurants/:id", (req, res)=>{
-    res.status(204).json({
-        status: "success",
-        data: {
-            restaurant: "mcdonald offbrand meal"
-        }
-    }) 
+app.delete("/api/v1/restaurants/:id", async(req, res)=>{
+    try {
+        const results = await db.query("DELETE FROM restaurants WHERE id = $1", [req.params.id])
+        res.status(204).json({
+            status: "success",
+            data: {
+                restaurant: results.rows[0]
+            }
+        });
+    } catch (error) {
+        console.log(error);
+    }
+    
 })
 
 const port = process.env.PORT || 4000;
